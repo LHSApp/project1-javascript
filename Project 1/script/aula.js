a705ebaba02dc358bd51edc8ad0dfa468bcc47dc
Leandro Santos <leandrosantos020788@gmail.com>
2021-07-21 22:53:40 -0300

    Update

--- Project 1/script/aula.js
+++ Project 1/script/aula.js
@@ -18,3 +18,26 @@ console.log(h2sAnimais);
 const paragrafos = document.querySelectorAll('p');
 
 console.log(paragrafos[paragrafos.length - 1]);
+
+FOREACH
+
+
+
+
+// Mostre no console cada parágrado do site
+
+// Mostre o texto dos parágrafos no console
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(item, index => {
+    console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach( => {
+    console.log(i++);
+});
+
+imgs.forEach(() => i++);
